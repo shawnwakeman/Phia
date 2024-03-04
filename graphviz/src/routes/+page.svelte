bda441918340d00348b0fcaadf70c52f8de62b46
Shawn Wakeman <wakemanshawn@gmail.com>
2024-03-04 15:36:53 -0600

    comm

--- graphviz/src/routes/+page.svelte
+++ graphviz/src/routes/+page.svelte
@@ -2,8 +2,8 @@
     import BarChart from "./BarChart.svelte"
   </script>
   
-  <main>
-    <h1>D3 Bar Chart in Svelte with TypeScript</h1>
-    <BarChart />
-  </main>
+
+<h1>D3 Bar Chart in Svelte with TypeScript</h1>
+<BarChart />
+
   
--- graphviz/src/routes/BarChart.svelte
+++ graphviz/src/routes/BarChart.svelte
@@ -1,35 +1,59 @@
 <script lang="ts">
     import { onMount } from 'svelte';
-    import { select } from 'd3';
-    import type { Selection } from 'd3';
+    import * as d3 from 'd3';
   
-    let svg: SVGSVGElement;
-  
+    interface Node {
+        radius: number;
+        value: number;
+        x?: number;
+        y?: number;
+        color?: string;
+
+    }
+    
+
     onMount(() => {
-      const data: number[] = [5, 15, 30, 2, 20]; // Sample data
-      const width: number = 400;
-      const barHeight: number = 20;
-  
-      const chart: Selection<SVGSVGElement, unknown, null, undefined> = select(svg)
+    const width: number = 1000, height: number = 1000;
+
+    
+    const colorScale: string[] = ['orange', 'lightblue', '#B19CD9'];
+
+    const numNodes: number = 50;
+    const nodes: Node[] = d3.range(numNodes).map((d, i) => ({
+      radius: Math.random() * 10,
+      value: Math.random(),
+      color: colorScale[i % colorScale.length], // Assign color based on index
+    }));
+
+    const simulation = d3.forceSimulation(nodes as d3.SimulationNodeDatum[])
+      .force('charge', d3.forceManyBody().strength(5))
+      .force('x', d3.forceX().x(() => width/2))
+      .force('y', d3.forceY().y(() => 100))
+      .force('collision', d3.forceCollide().radius((d: any) => d.radius))
+      .on('tick', ticked);
+
+
+    function ticked() {
+      const u = d3.select('svg g')
         .attr('width', width)
-        .attr('height', barHeight * data.length);
-  
-      const bar = chart.selectAll('g')
-        .data(data)
-        .enter().append('g')
-        .attr('transform', (d, i) => `translate(0,${i * barHeight})`);
-  
-      bar.append('rect')
-        .attr('width', d => d * 10) // Scale data
-        .attr('height', barHeight - 1);
-  
-      bar.append('text')
-        .attr('x', d => d * 10 - 3)
-        .attr('y', barHeight / 2)
-        .attr('dy', '.35em')
-        .text(d => d);
-    });
-  </script>
-  
-  <svg bind:this={svg}></svg>
-  
+        .selectAll('circle')
+        .data(nodes)
+        .join('circle')
+        .attr('r', (d: Node) => d.radius)
+        .attr('cx', (d: Node) => d.x ?? 0)
+        .attr('cy', (d: Node) => d.y ?? 0);
+    }
+  });
+</script>
+
+<style>
+    circle {
+      color: #8b2424;
+    }
+</style>
+    
+
+
+<svg width="600" height="400">
+  <g></g>
+</svg>
